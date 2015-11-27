c78bb1de6ab2123b667039f35c452ca0181cd8b5
luang008 <luangthu@gmail.com>
2015-11-26 19:11:16 -0500

    little comments added

--- batch_change_file_suffix.py
+++ batch_change_file_suffix.py
@@ -2,12 +2,14 @@ import sys
 import os
 import glob
 import shutil
-def batch_copy_file_change_suffix(in_dir,out_dir):
+def batch_copy_file_change_suffix(in_dir,out_dir, old_suffix, new_suffix):
     '''
-        I write this code to copy all the java source file to python files in another place
+        I write this code to copy all the java source file to empty python files in another place.
+        Similar function can be found in package shutil.
+        Usage: python batch_copy_file_change_suffix.py in_dir out_dir old_suffix new_suffix
     '''
-    OLD_SUFFIX = "txt"
-    NEW_SUFFIX = "csv"
+    OLD_SUFFIX = old_suffix
+    NEW_SUFFIX = new_suffix
     in_dir = os.path.abspath(in_dir)
     base_dir = os.path.abspath(out_dir)
     if not os.path.exists(base_dir):
@@ -16,7 +18,7 @@ def batch_copy_file_change_suffix(in_dir,out_dir):
         out_dir = base_dir + dirName[len(in_dir):]
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
-        for filename in filenames:
+        for filename in filenames: # Copy all files including metadata like .DS_Store
             if filename.endswith(OLD_SUFFIX):
                 filename = filename[:-len(OLD_SUFFIX)] + NEW_SUFFIX
             open(os.path.join(out_dir,filename),'w+').close()
@@ -24,4 +26,6 @@ def batch_copy_file_change_suffix(in_dir,out_dir):
 if __name__ == '__main__':
 	in_dir = sys.argv[1]
 	out_dir = sys.argv[2]
+    old_suffix = sys.argv[3]
+    new_suffix = sys.argv[4]
 	batch_copy_file_change_suffix(in_dir,out_dir)
